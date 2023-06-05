9fbbba02f549bd4d418ef1dc86449aff6a51e4db
Stefan Dharmawan <stefanwork12@gmail.com>
2023-06-05 23:46:06 +0700

    Update index.blade.php

--- resources/views/diagnosis/index.blade.php
+++ resources/views/diagnosis/index.blade.php
@@ -83,7 +83,7 @@
                                 </div>
                             @endforeach
                             <button id="btn_getResult"
-                                class="disabled:cursor-not-allowed disabled:bg-gray-400 inline-flex items-center px-4 py-2 bg-blue-700 dark:bg-blue-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-blue-800 uppercase tracking-widest hover:bg-blue-800 dark:hover:bg-white focus:bg-blue-700 dark:focus:bg-white active:bg-blue-900 dark:active:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
+                                class="w-full flex justify-center sm:w-max inline-flex sm:items-center disabled:cursor-not-allowed disabled:bg-gray-400  px-4 py-2 bg-blue-700 dark:bg-blue-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-blue-800 uppercase tracking-widest hover:bg-blue-800 dark:hover:bg-white focus:bg-blue-700 dark:focus:bg-white active:bg-blue-900 dark:active:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                                 type="submit" disabled>lihat hasil</button>
                         </form>
                     </div>
@@ -202,6 +202,9 @@
                             <li>Perlu diingat bahwa kuesioner yang akan Anda kerjakan adalah sebuah <b>alat
                                     tes</b>
                                 dan <b>TIDAK dapat menentukan</b> apabila Anda mengalami gangguan mental depresi.
+                            </li>
+                            <li>
+                                Apabila sudah mengisi menjawab semua pertanyaan, silakan klik tombol "lihat hasil".
                             </li>
                         </ul>
                     </div>
